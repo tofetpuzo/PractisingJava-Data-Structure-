17eb019f44e39a56e60c3939fad1c88197787dab
tofetpuzo <topetemiola09@gmail.com>
2022-02-02 11:52:01 +0000

    learnDataStructure

---
+++ GameEntry.java
@@ -0,0 +1,30 @@
+public class GameEntry {
+
+    private String name;
+    private int score;
+
+    public GameEntry(String n, int s) {
+        this.name = n;
+        this.score = s;
+    }
+
+    // returns the name field
+    public String getName() {
+        return name;
+    }
+
+    /** returns the score field */
+    public int getScore() {
+        return score;
+    }
+
+    /** returns a string representation of this entry */
+    public String toString() {
+        return "(" + name + ", " + score + ")";
+    }
+
+    /* Constructs a game entry with given parameters */
+    public static void main(String[] args) {
+
+    }
+}
