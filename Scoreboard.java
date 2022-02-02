55089aad61b4b896a3c2d573d599b77868406119
tofetpuzo <topetemiola09@gmail.com>
2022-02-02 12:28:35 +0000

    learnDataStructure

--- Scoreboard.java
+++ Scoreboard.java
@@ -1,7 +1,15 @@
 package PractiseJavaDataStructure;
+
 /**  */
 public class Scoreboard {
     private int numEntries = 0;
     private GameEntry[] board;
-    
+
+    /**
+     * constructs an empty scoreboard with the given capacity for storing entries
+     */
+    public Scoreboard(int capacity) {
+        board = new GameEntry[capacity];
+    }
+
 }
