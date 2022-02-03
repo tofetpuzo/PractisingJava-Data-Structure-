499e6e4c5b1f39a72988f875a5d6ab22499a196a
tofetpuzo <topetemiola09@gmail.com>
2022-02-03 12:27:48 +0000

    learnDataStructure

--- TetrisScoreboard.java
+++ TetrisScoreboard.java
@@ -1,5 +1,13 @@
 package PractiseJavaDataStructure;
 
+import java.util.Random;
+
+import javax.print.event.PrintJobListener;
+import javax.sound.sampled.SourceDataLine;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**  */
 public class TetrisScoreboard {
     private int numEntries = 0; // number of actual entries
@@ -38,27 +46,49 @@ public class TetrisScoreboard {
         if (i < 0 || i >= numEntries)
             throw new IndexOutOfBoundsException("Invalid: " + i);
         TetrisEntry temp = board[i]; // keep the object which will be removed...
-        for (int j = i; j < numEntries - 1; i++) { //This iteration helps us count score from where we want to remove
+        for (int j = i; j < numEntries - 1; i++) { // This iteration helps us count score from where we want to remove
             board[j] = board[j + 1];
             board[numEntries - 1] = null;
-        } numEntries--;
+        }
+        numEntries--;
         return temp;
     }
 
-    /** Insertion-sort of an array of characters into non-descending  */
-        public static void insertionSort(char[] data){
-            int n = data.length;
-            for(int k = 1; k < n; k++) {  //begin with second character
-                char current_value = data[k]; //time to insert current_value
-                int j = k;  // insert the exact index j for char current_value
-                while( j > 0 && data[j - 1] > current_value){ //thus data[j -1] must go after cuurent_value
-                    data[j] = data[j -1]; //
-                    j --;
-                }
-                data[j] = current_value;
+    /** Insertion-sort of an array of characters into non-descending */
+    public static void insertionSort(char[] data) {
+        int n = data.length;
+        for (int k = 1; k < n; k++) { // begin with second character
+            char current_value = data[k]; // time to insert current_value
+            int j = k; // insert the exact index j for char current_value
+            while (j > 0 && data[j - 1] > current_value) { // thus data[j -1] must go after cuurent_value
+                data[j] = data[j - 1]; //
+                j--;
             }
-             
+            data[j] = current_value;
+        }
+
     }
 
+    /** a code to illustrate the Array sorting using pseudocode */
+    public class TestingArraySorting {
+        public static void main(String[] args) {
+            int dataset[] = new int[10];
+            Random randoms = new Random(); // this generates pseudorandom numbers at run time
+            randoms.setSeed(System.currentTimeMillis()); // I used the current time of any system running this program
+            for (int i = 0; i < dataset.length; i++) {
+                dataset[i] = randoms.nextInt(100);
+                int[] original_copy = Arrays.copyOf(dataset, dataset.length);
+                System.out.println("Arrays are equal before sorting: " + Arrays.equals(dataset, original_copy));
+                Arrays.sort(dataset);
+                System.out.println();
+                System.out.println("Arrays are equal after sorting: " + Arrays.equals(dataset, original_copy));
+                System.out.println("original_copy= " + Arrays.toString(original_copy));
+                System.out.println("original_copy= " + Arrays.toString(dataset));
+
+            }
+
+        }
+
+    }
 
 }
