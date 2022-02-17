3713669e906e3d3e72d2b5be7066ad5fde9f5e4f
tofetpuzo <topetemiola09@gmail.com>
2022-02-17 08:49:28 +0000

    learnDataStructure

--- Matrices.java
+++ Matrices.java
@@ -12,25 +12,21 @@ public class Matrices {
     // ArrayList matArows = new ArrayList();
 
     void buildMatrices() {
-        Scanner row = new Scanner(System.in);
-        Scanner col = new Scanner(System.in);
-        Scanner dataA = new Scanner(System.in)
-        Scanner dataB = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+ 
         System.out.print("Enter the first matrix dimension value: ");
-        int numsrowA = row.nextInt();
+        rows = input.nextInt();
         // System.out.print("You want a matrix of " + numbs1);
         System.out.print("Enter the second matrix dimension value: ");
-        int numscolA = col.nextInt();
-        rows = numsrowA;
-        cols = numscolA;
+        cols = input.nextInt();;
         int[][] numbers = new int[rows][cols];
 
         for (int i = 0; i < numbers.length; i++) {
             System.out.println("Enter numbers for the rows in the matrix: ");
-            int dataforrowsMatA = dataA.nextInt();
+            int dataforrowsMatA = input.nextInt();
             matArows.add(dataforrowsMatA);
             System.out.println("Enter numbers for the digit in cols matrix: ");
-            int dataforcolsMatA = dataB.nextInt();
+            int dataforcolsMatA = input.nextInt();
             matAcols.add(dataforcolsMatA);
             // for (int j = 0; j < numbers[0].length - 1; j++) {
             // //System.out.println("Enter numbers for the cols in the matrix: ");
